7b8733f4bcc3c4ad9cebf9b0c050fad20d2d4910
Mousecop <joaskew13@gmail.com>
2017-03-14 11:55:21 -0400

    Game component displaying, psuedo for components

---
+++ js/components/gameContainer.js
@@ -0,0 +1,22 @@
+/*
+//list of components
+  input component
+  new game component
+  status component
+  guess button component
+  guess count component
+  guess history component
+  info component
+  play container component
+
+*/
+
+import React from 'react';
+
+export default function Game(props) {
+  return (
+    <div className="gameContainer">
+      <h1 className="header">Hot or Cold</h1>
+    </div>
+  );
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import Game from './components/gameContainer';
+
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(<Game />, document.getElementById('root'))
+);
