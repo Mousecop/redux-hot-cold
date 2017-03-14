d8bdda0fefa36d210451aefe225d772e6827f122
Mousecop <joaskew13@gmail.com>
2017-03-14 15:45:43 -0400

    add reset from function

--- js/components/userSubmit.js
+++ js/components/userSubmit.js
@@ -7,14 +7,15 @@ import store from '../store';
 export function UserSubmit(props) {
     let userInput;
   return (
-    <form className="user-submit" onSubmit={e => {
+    <form id="form" className="user-submit" onSubmit={e => {
             e.preventDefault();
-            props.dispatch(actions.makeGuess(userInput.value))}}>
+            props.dispatch(actions.makeGuess(userInput.value));
+            const form = document.getElementById('form');
+            form.reset();}}>
         <input ref={ref => userInput=ref} type="text" placeholder="Enter Your Guess" maxLength="3" required />
         <input type="submit" value="guess"/>
     </form>
   );
-
 }
 
 export default connect()(UserSubmit);
