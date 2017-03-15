6c75451aa4054b7b1e5f21235acc261a58f2b305
Dave Hoel <dkhoel@gmail.com>
2017-03-15 09:23:33 -0700

    adds showModal element and switch logic

--- js/components/gameContainer.js
+++ js/components/gameContainer.js
@@ -11,18 +11,32 @@
 */
 
 import React from 'react';
-
+import {connect} from 'react-redux';
 import PlayContainer from './playContainer';
 import NewGame from './newGame';
 import ShowModal from './showModal';
+import * as actions from '../actions/index';
+import store from '../store';
 
-export default function Game(props) {
+export function Game(props) {
+    let toggle = 'none';
+    if (props.modalToggle) {
+        toggle = 'block';
+    }
   return (
     <div className="gameContainer">
       <h1 className="header">Hot or Cold</h1>
       <NewGame />
       <ShowModal />
       <PlayContainer />
+      <div id="sm" style={{display: toggle}}>
+          <h2>Show Modal</h2>
+          <button onClick={() => {props.dispatch(actions.toggleInfoModal())}}>Got it!</button>
+      </div>
     </div>
   );
 }
+const mapStateToProps = (state, props) => ({
+    modalToggle: state.showInfoModal
+});
+export default connect(mapStateToProps)(Game);
